e008885341b2f2145f489e82aeeba54363bf35b7
Jeong Ye Bin <dpqls724@gmail.com>
2023-06-01 14:35:45 +0900

    page 수정작업중

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ import Header from "./components/Header/Header";
 import styles from "./App.module.css";
 
 function App() {
+  const screenSize = () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  };
+
+  window.addEventListener("resize", () => screenSize());
+
   return (
     <div className={styles.wrap}>
       <Header />
