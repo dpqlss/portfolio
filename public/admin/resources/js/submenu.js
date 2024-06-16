79687ac7e6e3758d66494a5c6b642a346a9f0b13
Jeong Ye Bin <dpqls724@gmail.com>
2024-06-16 09:17:33 +0900

    fix: 포트폴리오 내용 수정

--- public/admin/resources/js/submenu.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    $('#userDropdown').click(function(event) {
-        event.preventDefault(); // 기본 이벤트 방지
-        $('.dropdown-menu').toggle(); // display 속성을 토글
-    });
-});
